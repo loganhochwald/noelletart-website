e921d3cf85dd522a54de83eec9e953176adefde2
logan <loganh42@gmail.com>
2023-08-13 14:09:46 -0400

    Add Basic React Setup Files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <p>HI Noelle, React is Working!</p>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('app');
+
+// HTMLElement | null; had to add error handling or has an error
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+};
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <App />
+);
