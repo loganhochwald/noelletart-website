f14dbb5bcd5db1e7ae436744283efd72e98c9b9c
logan <loganh42@gmail.com>
2023-08-13 18:27:49 -0400

    Move Header Styles to Header Styles Folder

--- src/Global/Header/Header.tsx
+++ src/Global/Header/Header.tsx
@@ -1,29 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const HeaderWrapper = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem 0;
-`;
-
-const HeaderTitle = styled.h1`
-  margin: 0;
-`;
-
-const LinksWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-  margin-top: 1rem;
-`;
-
-const LinkItem = styled.h3`
-  text-decoration: none;
-  font-weight: bold;
-`;
+import { HeaderWrapper, HeaderTitle, LinksWrapper, LinkItem } from './HeaderStyle';
 
 const Header = () => {
   return (
---
+++ src/Global/Header/HeaderStyle.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem 0;
+`;
+
+export const HeaderTitle = styled.h1`
+  margin: 0;
+`;
+
+export const LinksWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  margin-top: 1rem;
+`;
+
+export const LinkItem = styled.h3`
+  text-decoration: none;
+  font-weight: bold;
+`;
+
+module.exports = {
+  HeaderWrapper,
+  HeaderTitle,
+  LinksWrapper,
+  LinkItem
+}
