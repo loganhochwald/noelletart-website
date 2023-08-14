806e6d28ad6c37253f0d826300198cdf98174c50
logan <loganh42@gmail.com>
2023-08-13 22:17:19 -0400

    Edit Route Endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Artwork />} />
-        <Route path="about/" element={<About />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
