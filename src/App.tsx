1cfff16a3cafa5a5970bf11bc620e8cfb4b77fc1
loganhochwald <loganh42@gmail.com>
2023-08-26 15:39:33 -0500

    Add Containers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 import Header from './Global/Header/Header';
 import Artwork from './pages/Artwork/Artwork';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 
+const AppContainer = styled.div`
+margin: 2rem;
+`
+
+const RoutesContainer = styled.div`
+margin: 1rem;
+`
+
 const App = () => {
 
   return (
-    <>
+    <AppContainer>
       <Header />
+      <RoutesContainer>
       <Routes>
         <Route path="/" element={<Artwork />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+      </RoutesContainer>
+    </AppContainer>
   );
 };
 
