632b896c9575747e845419c136393ec556526e9a
loganhochwald <loganh42@gmail.com>
2023-08-26 15:39:16 -0500

    Add Bio to About Page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
     <AboutGridContainer>
       <AboutImageContainer>
         <NoelleImage src={noellePic} alt="Noelle Image" />
-        <p>Noelle's About Info goes into here!</p>
+        <p>Noelle Tollett is an emerging interdisciplinary painter based in New Orleans. Originally from Baton Rouge, she graduated magna cum laude from LSU, earning her BFA in Studio Art with concentration in Painting and Drawing. Since moving to New Orleans, Noelle teaches children's art classes, live paints at music shows, volunteers as a set designer and performer with AquaMob, and hosts figure drawing classes.</p>
       </AboutImageContainer>
       <AboutBioContainer>
         <p>View CV and Download CV Links go Here</p>
