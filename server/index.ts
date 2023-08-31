8db4a123c4ca9828f5f1f541584efbd2b23afd69
loganhochwald <loganh42@gmail.com>
2023-08-31 18:05:33 -0500

    Add Route for s3 List Objects

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { AWS } from './routes/aws';
 import setupMiddleware from './middleware';
 
 // load variables from .env file
@@ -8,6 +9,8 @@ dotenv.config();
 // server instance and port
 const app = express();
 const port = 4200;
+
+app.use('/aws', AWS);
 
 // Call the setupMiddleware function and pass the app instance
 setupMiddleware(app);
