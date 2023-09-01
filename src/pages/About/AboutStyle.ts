08e4ddaa7f413f85f11343652a17337fa728b05d
Logan Hochwald <loganhochwald@gmail.com>
2023-09-01 15:36:47 -0500

    About Bio Text Justify and Justify Items Center

--- src/pages/About/AboutStyle.ts
+++ src/pages/About/AboutStyle.ts
@@ -3,20 +3,19 @@ import styled from 'styled-components';
 const AboutGridContainer = styled.div`
 display: grid;
 grid-template-columns: 1fr;
-grid-template-rows: repeat(2, 1fr);
-grid-row-gap: 0px;
 border: 1px solid #ccc;
 `;
 
 const AboutImageContainer = styled.div`
 align-items: center;
 display: grid;
-grid-template-columns: repeat(2, 1fr);
 grid-template-rows: 1fr;
 border: 1px solid #ccc;
 padding: 20px;
 margin: 10px;
 box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+justify-items: center;
+text-align: justify;
 `
 
 const NoelleImage = styled.img`
