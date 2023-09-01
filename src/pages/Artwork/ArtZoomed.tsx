1f681a500cffde5b018ef1942b11b6364a3971cf
loganhochwald <loganh42@gmail.com>
2023-08-31 19:32:10 -0500

    Edit Styling and Add Button to ArtZoomed

--- src/pages/Artwork/ArtZoomed.tsx
+++ src/pages/Artwork/ArtZoomed.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { ArtZoomedContainer, ArtZoomedImage, ArtZoomedTitle, ArtZoomedButton } from "./ArtZoomedStyle";
 
 const ArtZoomed = () => {
+  const navigate = useNavigate();
+
+  // Grab the image URL and title from the location state sent when navigating to this page
+  const location = useLocation();
+  const url = location.state?.pictureURL;
+  const imageTitle = location.state?.title;
+
   return (
-    <p>Hee Hee</p>
+      <ArtZoomedContainer>
+        <ArtZoomedImage src={url}/>
+        <ArtZoomedTitle>{imageTitle}</ArtZoomedTitle>
+        <ArtZoomedButton onClick={() => navigate('/')}>Return to Gallery</ArtZoomedButton>
+      </ArtZoomedContainer>
   );
 };
 
---
+++ src/pages/Artwork/ArtZoomedStyle.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const ArtZoomedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ArtZoomedImage = styled.img`
+  max-width: 100%;
+  height: auto;
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const ArtZoomedTitle = styled.h2`
+`;
+
+export const ArtZoomedButton = styled.button`
+margin-top: 0.5rem
+`
