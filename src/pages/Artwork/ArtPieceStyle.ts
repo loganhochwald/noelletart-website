43c02ca008c70189e1e9669151a5efe011f33a56
loganhochwald <loganh42@gmail.com>
2023-09-04 12:45:21 -0500

    Remove Borders from Artwork

--- src/pages/Artwork/ArtPieceStyle.ts
+++ src/pages/Artwork/ArtPieceStyle.ts
@@ -4,17 +4,14 @@ export const ArtPieceContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border: 1px solid #ccc;
   padding: 2rem;
   margin: 1rem;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   justify-content: flex-end;
 `;
 
 export const ArtPieceImage = styled.img`
   max-width: 100%;
   height: auto;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   cursor: pointer;
   border: solid 1px rgb(0 0 0);
 `;
--- src/pages/Artwork/ArtworkStyle.ts
+++ src/pages/Artwork/ArtworkStyle.ts
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const ArtGridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-  gap: 1.6rem;
   margin: 0 2rem;
   justify-content: center;
-  lign-items: center;
+  align-items: flex-start;
   `;
