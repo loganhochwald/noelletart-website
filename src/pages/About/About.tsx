82ab4a3d7beacf9922319f55dfa43b72a4cc8c36
loganhochwald <loganh42@gmail.com>
2023-09-04 16:51:37 -0500

    Format About Page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { AboutGridContainer, AboutImageContainer, NoelleImage,
-  AboutBioContainer, } from './AboutStyle';
+import { AboutGridContainer, AboutImageContainer, NoelleImage, AboutInfoContainer,
+  AboutBioContainer, AboutContAndInfoContainer, VIEWCV, ContactInfo, UnderlinedText, UnderlinedLink
+} from './AboutStyle';
 import noelle from './Noelle-Headshot.webp'
 
 const About = () => {
@@ -10,12 +11,19 @@ const About = () => {
       <AboutImageContainer>
         <NoelleImage src={noelle} alt="Noelle Image" />
       </AboutImageContainer>
+      <AboutInfoContainer>
       <AboutBioContainer>
         NOELLE TOLLETT is an emerging interdisciplinary painter based in New Orleans, LA. Originally from Baton Rouge, LA,
         she graduated magna cum laude from Louisiana State University, earning her Bachelor's of Fine Arts in Studio Art with 
         a concentration in Painting and Drawing. Since moving to New Orleans, Noelle teaches children's art classes, live 
         paints at music shows, and volunteers as a set designer and performer with AquaMob New Orleans.
       </AboutBioContainer>
+      <AboutContAndInfoContainer>
+      <VIEWCV><UnderlinedText>VIEW CV</UnderlinedText></VIEWCV>
+      <ContactInfo>INSTAGRAM: <UnderlinedLink href="https://www.instagram.com/noelletart">@noelletart</UnderlinedLink></ContactInfo>
+      <ContactInfo>EMAIL: <UnderlinedLink href="mailto:noelletart@gmail.com">noelletart@gmail.com</UnderlinedLink></ContactInfo>
+    </AboutContAndInfoContainer>
+      </AboutInfoContainer>
     </AboutGridContainer>
   );
 };
--- src/pages/About/AboutStyle.ts
+++ src/pages/About/AboutStyle.ts
@@ -1,37 +1,79 @@
 import styled from 'styled-components';
 
+const breakpoint = '1000px';
+
 const AboutGridContainer = styled.div`
 display: flex;
 flex-direction: row;
-align-items: flex-start;
-border: 1px solid #ccc;
-padding: 20px;
-max-width: 100%;
+padding: 2rem;
+max-width: 900px;
 box-sizing: border-box;
 flex-wrap: wrap;
+margin: auto;
+column-gap: 1rem;
+row-gap: 1rem;
+
+@media (max-width: ${breakpoint}) {
+  flex-direction: column;
+}
 `;
 
 const AboutImageContainer = styled.div`
 max-width: 100%;
 height: auto;
-box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 flex: 1;
+display: flex;
 `;
 
 const NoelleImage = styled.img`
   max-width: 100%;
   height: auto;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  min-width: 220px;
+  border: solid black;
+  border-radius: 2%;
+`;
+
+const AboutInfoContainer = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
 `;
 
 const AboutBioContainer = styled.div`
-flex: 2;
-padding: 0 20px;
 `;
+
+const AboutContAndInfoContainer = styled.div`
+`;
+
+const VIEWCV = styled.div`
+text-align: center;
+margin: 2rem;
+`;
+
+const ContactInfo = styled.div`
+`;
+
+const UnderlinedText = styled.span`
+  text-decoration: underline;
+`;
+
+const UnderlinedLink = styled.a`
+  text-decoration: underline;
+  color: black;
+`;
+
 
 export {
   AboutGridContainer,
   AboutImageContainer,
   NoelleImage,
+  AboutInfoContainer,
   AboutBioContainer,
+  AboutContAndInfoContainer,
+  VIEWCV,
+  ContactInfo,
+  UnderlinedText,
+  UnderlinedLink,
 }
