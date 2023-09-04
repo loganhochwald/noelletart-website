4b9dcbb1883977c62249f3930d7eae8b4d380cbc
loganhochwald <loganh42@gmail.com>
2023-09-04 12:03:14 -0500

    Add File Loader to Production

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,6 +34,13 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(png|jpe?g|gif|jp2|webp)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
     ],
   },
   resolve: {
