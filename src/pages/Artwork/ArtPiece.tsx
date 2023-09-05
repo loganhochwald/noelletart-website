e0e87ea88e1dc8da804b5a4d4c82d8cc723d4937
Logan Hochwald <loganhochwald@gmail.com>
2023-09-05 17:37:55 -0500

    Add Video Handling

--- src/pages/Artwork/ArtPiece.tsx
+++ src/pages/Artwork/ArtPiece.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { ArtPieceContainer, ArtPieceImage, ArtPieceTitle } from './ArtPieceStyle';
+import { ArtPieceContainer, ArtPieceImage, ArtPieceTitle, ArtPieceVideo } from './ArtPieceStyle';
+import e from 'express';
 
 interface ArtPieceProps {
   src: string;
@@ -9,16 +10,27 @@ interface ArtPieceProps {
 const ArtPiece: React.FC<ArtPieceProps> = ({ src }) => {
   const navigate = useNavigate();
   const imageFilename = src.split('/').pop() || '';
-  const imageTitle = imageFilename.replace(/\.[^.]+$/, '');  
+  const imageTitle = imageFilename.replace(/\.[^.]+$/, '');
+  
+  const [isVideo, setIsVideo] = useState<boolean>(false);
 
   const zoomIn = (picture: string) => {
     navigate(`/artpiece/${imageTitle}`, { state: { pictureURL: picture, title: imageTitle } });
   }
 
+  useEffect(() => {
+    if(!imageFilename.endsWith('.jpg')) {
+      console.log(imageFilename);
+      setIsVideo(true);
+    } else {
+      setIsVideo(false);
+    }
+  }, [src]);
+
   return (
     <ArtPieceContainer>
-      <ArtPieceImage src={src} alt="Artwork Image" onClick={() => zoomIn(src)}/>
-      <ArtPieceTitle>{imageTitle}</ArtPieceTitle>
+      {isVideo ? <ArtPieceVideo src={src} controls></ArtPieceVideo> : <ArtPieceImage src={src} alt="Artwork Image" onClick={() => zoomIn(src)}/>}
+    <ArtPieceTitle>{imageTitle}</ArtPieceTitle>
     </ArtPieceContainer>
   );
 };
--- src/pages/Artwork/ArtPieceStyle.ts
+++ src/pages/Artwork/ArtPieceStyle.ts
@@ -21,3 +21,8 @@ margin-top: 1.5rem;
 margin-bottom: 0rem;
 text-align: center;
 `
+export const ArtPieceVideo = styled.video`
+  max-width: 100%;
+  height: auto;
+  border: solid 1px rgb(0 0 0);
+`;
