26b952e2b823e55bf044ca8f68cd68f5bf059f02
loganhochwald <loganh42@gmail.com>
2023-09-05 15:51:10 -0500

    Add Global Loader

--- src/Global/Header/HeaderStyle.ts
+++ src/Global/Header/HeaderStyle.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const HeaderWrapper = styled.header`
   display: flex;
@@ -36,4 +36,40 @@ export const LinkItem = styled.h3`
     color:#006e90;
     transition: all 0.3s ease;
   }
+`;
+
+const rotateAnimation = keyframes`
+  0% {
+    border-width: 10px;
+  }
+  25% {
+    border-width: 3px;
+  }
+  50% {
+    transform: rotate(115deg);
+    border-width: 10px;
+  }
+  75% {
+    border-width: 3px;
+  }
+  100% {
+    border-width: 10px;
+  }
+`;
+
+export const LoaderContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+export const Loader = styled.div`
+  width: 70px;
+  height: 70px;
+  border: 5px solid #fae3e8;
+  border-color: #fcfcff #fcfcff #fae3e8 #fae3e8;
+  border-radius: 50%;
+  box-sizing: border-box;
+  animation: ${rotateAnimation} 3s ease-in-out infinite;
+  transform: rotate(-200deg);
 `;
