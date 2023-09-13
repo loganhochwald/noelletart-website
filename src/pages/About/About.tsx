093b08d542dcecedbad02730fb3e304622562ab2
Logan Hochwald <loganhochwald@gmail.com>
2023-09-12 19:51:06 -0500

    Edit About to Call Both Bio and About from S3 in Main Component

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,42 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { AboutGridContainer, AboutImageContainer, NoelleImage, AboutInfoContainer,
   AboutBioContainer, AboutStatementContainer, AboutContAndInfoContainer,
   VIEWCV, ContactInfo, UnderlinedText, UnderlinedLink
 } from './AboutStyle';
 import noelle from './Noelle-Headshot.webp';
-import cv from './Noelle-CV.pdf';
+import { LoaderContainer, Loader } from '../../Global/Header/HeaderStyle';
+import axios from "axios";
 
-import Bio from './Bio';
-import AboutStatement from './AboutStatement';
 
 const About = () => {
 
-  const openPdf = () => {
-    window.open(cv, '_blank');
-  };
+  const [loading, setLoading] = useState(true);
+  const [about, setAbout] = useState('');
+  const [bio, setBio] = useState('');
+
+  useEffect(() => {
+    // wait for both API calls to complete
+    Promise.all([
+      axios.get('/.netlify/functions/about'),
+      axios.get('/.netlify/functions/bio')
+    ])
+    .then(([aboutResponse, bioResponse]) => {
+      setAbout(aboutResponse.data);
+      setBio(bioResponse.data);
+       // set loading to false when both data are loaded
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error("Error loading data", error);
+      setLoading(false);
+    });
+  }, []);
 
   const openS3Pdf = () => {
     window.open('https://noelletart.s3.amazonaws.com/cv.pdf');
   }
 
+  if (loading) {
+    return <LoaderContainer><Loader/></LoaderContainer>;
+  }
+
   return (
     <AboutGridContainer>
       <AboutStatementContainer>
-        <AboutStatement />
-      </AboutStatementContainer>
-      <AboutImageContainer>
-        <NoelleImage src={noelle} alt="Noelle Image" />
-      </AboutImageContainer>
-      <AboutInfoContainer>
-      <AboutBioContainer>
-        <Bio />
-      </AboutBioContainer>
-      <AboutContAndInfoContainer>
-      <VIEWCV onClick={openS3Pdf}><UnderlinedText>VIEW CV</UnderlinedText></VIEWCV>
-      <ContactInfo>INSTAGRAM: <UnderlinedLink href="https://www.instagram.com/noelle.tart">@noelletart</UnderlinedLink></ContactInfo>
-      <ContactInfo>EMAIL: <UnderlinedLink href="mailto:noelletart@gmail.com">noelletart@gmail.com</UnderlinedLink></ContactInfo>
-    </AboutContAndInfoContainer>
-      </AboutInfoContainer>
+      { about }
+    </AboutStatementContainer>
+    <AboutImageContainer>
+      <NoelleImage src={noelle} alt="Noelle Image" />
+    </AboutImageContainer>
+    <AboutInfoContainer>
+    <AboutBioContainer>
+      { bio }
+    </AboutBioContainer>
+    <AboutContAndInfoContainer>
+    <VIEWCV onClick={openS3Pdf}><UnderlinedText>VIEW CV</UnderlinedText></VIEWCV>
+    <ContactInfo>INSTAGRAM: <UnderlinedLink href="https://www.instagram.com/noelle.tart">@noelletart</UnderlinedLink></ContactInfo>
+    <ContactInfo>EMAIL: <UnderlinedLink href="mailto:noelletart@gmail.com">noelletart@gmail.com</UnderlinedLink></ContactInfo>
+  </AboutContAndInfoContainer>
+    </AboutInfoContainer>
     </AboutGridContainer>
   );
 };
